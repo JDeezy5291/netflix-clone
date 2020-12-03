d4e4dd48a0e11ed13cfc82d65fd6e7c2014f658b
JDeezy5291 <joshdemaria@aol.com>
2020-12-03 17:03:29 -0500

    header

--- src/App.js
+++ src/App.js
@@ -6,19 +6,12 @@ import requests from './requests'
 function App() { 
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* nav */}
-      {/* banner */}
       <Banner />
       <Row 
       title="Netflix Originals" 
       fetchUrl={requests.fetchNetflixOriginals}
       isLargeRow 
       />
-=======
-      <h1>Title here</h1>
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
->>>>>>> 2881193b372305b474883688347b16d718c7884d
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
