6e3d6e831f33baad95ef3d185116c833cd385003
JDeezy5291 <joshdemaria@aol.com>
2020-12-03 16:54:56 -0500

    header

--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios'
-import requests from './requests'
+import "./Row.css"
 
-function Row({ title, fetchUrl }) {
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+function Row({ title, fetchUrl, isLargeRow }) {
     // state is like short term memory; when refresh it disappears
     const [movies, setMovies] = useState([])
     
     // when row loads, make a request to tmdb 
     useEffect(() => {
-        // if [], run once when the row loads and don't run again
+        // if [], run once when the row loads and doesn't run again
         async function fetchData() {
             const request = await axios.get(fetchUrl)
             console.log(request.data.results)
@@ -18,13 +20,20 @@ function Row({ title, fetchUrl }) {
         fetchData()
     }, [fetchUrl])
 
-    console.log(movies)
+    console.table(movies)
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
-
-            
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img 
+                    key={movie.id}
+                    className={`row_poster ${isLargeRow && "row_posterLarge"}`} 
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                    alt={movie.name}/>
+                ))}
+            </div>
         </div>
     )
 }
